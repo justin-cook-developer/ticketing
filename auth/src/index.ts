eeee052217eb923cc5c4ecd79f3ec7ec80df89b1
Justin Cook <justin.cook@berkeley.edu>
2023-03-18 14:08:42 -0600

    Configure the Ingress Service for K cluster

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+    res.send("Hi there");
+});
+
 const PORT = process.env.AUTH_PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Auth listening on port: `, PORT);
+    console.log("Auth Listening on Port:", PORT);
 });
